03255a12a1dc8d2c9ca43edbd82613605de8d3d5
Shitanshukumar607 <147914822+Shitanshukumar607@users.noreply.github.com>
2023-12-02 23:02:33 +0530

    Add files via upload

---
+++ tab.js
@@ -0,0 +1,107 @@
+let Slider = document.getElementById("slider")
+let Heading = document.getElementById("heading")
+let Content = document.getElementById("content")
+
+let Div1 = document.getElementById("div1")
+let Div2 = document.getElementById("div2")
+let Div3 = document.getElementById("div3")
+let Div4 = document.getElementById("div4")
+let Div5 = document.getElementById("div5")
+
+let Icon1 = document.getElementById("icon1")
+let Icon2 = document.getElementById("icon2")
+let Icon3 = document.getElementById("icon3")
+let Icon4 = document.getElementById("icon4")
+let Icon5 = document.getElementById("icon5")
+
+function clicked1() {
+    let styleEl = "0px"
+    console.log(styleEl)
+    Slider.style.top = styleEl;
+    Div1.style.color = "white";
+    Div2.style.color = "black";
+    Div3.style.color = "black";
+    Div4.style.color = "black";
+    Div5.style.color = "black";
+    Icon1.style.color = "white";
+    Icon2.style.color = "black";
+    Icon3.style.color = "black";
+    Icon4.style.color = "black";
+    Icon5.style.color = "black";
+
+    Heading.textContent = "Home Content";
+    Content.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, adsimilique. Exercitationem unde, iure maiores cumque repellendusnesciunt libero in rerum recusandae dolor omnis ab, pariatur sitmodi magnam adipisci."
+}
+
+function clicked2() {
+    let styleEl = "37px"
+    Slider.style.top = styleEl;
+    Div1.style.color = "black";
+    Div2.style.color = "white";
+    Div3.style.color = "black";
+    Div4.style.color = "black";
+    Div5.style.color = "black";
+    Icon1.style.color = "black";
+    Icon2.style.color = "white";
+    Icon3.style.color = "black";
+    Icon4.style.color = "black";
+    Icon5.style.color = "black";
+
+    Heading.textContent = "Blog Content"
+    Content.textContent = " hg;ergpog  n  ghor     jg jv eor  hiorh  h 3I4T HT YI Y0R YYR Y A R HO HERI HO  AIGHHTITRA T7B3AboLI VJFH FVNJVN8;qg hncrrghcnorgmirmrorothnrm  hnrmr vhtnrhopmot "
+}
+
+function clicked3() {
+    let styleEl = "75px"
+    Slider.style.top = styleEl;
+    Div1.style.color = "black";
+    Div2.style.color = "black";
+    Div3.style.color = "white";
+    Div4.style.color = "black";
+    Div5.style.color = "black";
+    Icon1.style.color = "black";
+    Icon2.style.color = "black";
+    Icon3.style.color = "white";
+    Icon4.style.color = "black";
+    Icon5.style.color = "black";
+
+    Heading.textContent = "Help Content"
+    Content.textContent = " Lorem ipsum dolor, siametconsectetur adipisicing elit. A, adsimilique. Exercitationem unde, iure maiores cumque repellendusnesciunt libero in rerum recusandae dolor omnis ab, pariatur sitmodi magnam adipisci.Lorem ipsum dolor, sit amet consecteturLorem ipsum dolor, sit amet consecteturLorem ipsum dolor, sit amet consectetur"
+}
+
+function clicked4() {
+    let styleEl = "113px"
+    Slider.style.top = styleEl;
+    Div1.style.color = "black";
+    Div2.style.color = "black";
+    Div3.style.color = "black";
+    Div4.style.color = "white";
+    Div5.style.color = "black";
+    Icon1.style.color = "black";
+    Icon2.style.color = "black";
+    Icon3.style.color = "black";
+    Icon4.style.color = "white";
+    Icon5.style.color = "black";
+
+    Heading.textContent = "Code Content"
+    Content.textContent = " Lorem ipsum dolor, siametconsectetur adipisicing elit. A, adsimilique. Exercitationem unde, iure maiores cumque  sitmodi magnam adipisci.Lorem ipsum dolor, sit amet consecteturLorem nqerfgfiqlrgnnnmqn3rcl3RRRRRRRRRRRRRRRRRRRRRRRRRRRRRR H R "
+}
+
+function clicked5() {
+    let styleEl = "151px"
+    Slider.style.top = styleEl;
+    Div1.style.color = "black";
+    Div2.style.color = "black";
+    Div3.style.color = "black";
+    Div4.style.color = "black";
+    Div5.style.color = "white";
+    Icon1.style.color = "black";
+    Icon2.style.color = "black";
+    Icon3.style.color = "black";
+    Icon4.style.color = "black";
+    Icon5.style.color = "white";
+
+    Heading.textContent = "About Content"
+    Content.textContent = " Lorem ipsum dolor, siametconsectetur adipisicingT483LOI LRGOEIRGOQ3RNweferfr4 nqerfgfiqlrgnnnmqn3rcl3RRRRRRRRRRRRRRRRRRRRRRRRRRRRRR H R "
+}
+
